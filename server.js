ed8bdb2a2d6ba390c824445722a2d26a55d2bca2
dediacc <dedisuhaimiacc@gmail.com>
2020-09-11 12:27:56 -0700

    version final

--- server.js
+++ server.js
@@ -1,41 +1,54 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const passport = require('passport')
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+require("dotenv").config();
 
-const usersRoute = require('./routes/api/users')
-const notesRoute = require('./routes/api/notes')
+const usersRoute = require("./routes/api/users");
+const notesRoute = require("./routes/api/notes");
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
-app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(bodyParser.json());
 
-app.use(express.static(path.resolve(__dirname, "client", "build")));
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-}); 
 
-const db = process.env.DB_HOST
+const db = process.env.MONGO_URI;
 
-mongoose.connect(db, {
+mongoose
+.connect(db, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log("MongoDB connected")
-}).catch(err => {
-    console.error(err)
+    useUnifiedTopology: true,
 })
+  .then(() => {
+      console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.error(err);
+});
 
-app.use(passport.initialize())
+app.use(passport.initialize());
 
-require('./config/passport')(passport)
+require("./config/passport")(passport);
 
-app.use('/api/users', usersRoute)
-app.use('/api/notes', notesRoute)
+app.use("/api/users", usersRoute);
+app.use("/api/notes", notesRoute);
 
-const port = process.env.PORT || 5000
-app.listen(port, () => console.log(`Server and database running on ${port}`)) 
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server and database running on ${port}`));
